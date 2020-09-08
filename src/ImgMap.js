121222c83a9117a60149113a1d01fc2abda0b843
John Martin <jrmartin182@gmail.com>
2020-09-08 10:22:55 -0600

    area mapping done fore main image sections

--- src/ImgMap.js
+++ src/ImgMap.js
@@ -8,13 +8,36 @@ import solvay from './images/solvay-conf.jpg';
     const classes = useStyles();
     return (
         <div>
-            <figure>
+            <map name="map-overlay">
+                <area 
+                    alt="Section one."
+                    shape="rect" 
+                    href="#"
+                    coords="0,0,250,400"/>
+                <area 
+                    alt="Section two."
+                    shape="rect" 
+                    href="#"
+                    coords="250, 0, 480, 400"/>
+                <area 
+                    alt="Section three."
+                    shape="rect"
+                    href="#" 
+                    coords="480, 0, 775, 400"/>
+                <area 
+                    alt="Section four."
+                    shape="rect" 
+                    href="#"
+                    coords="775, 0, 1000,400 "/>
+            </map>
+            {/* <figure> */}
                 <img 
                     src={solvay}
-                    className={classes.solvay} 
-                    alt="A group photo of scientists from the 1927 Solvay Conference"/>
-                <figcaption>Scientists from the 1927 Solvay Conference.</figcaption>
-            </figure>
+                    className={classes.solvay}
+                    usemap="#map-overlay" 
+                    alt="A group photo of scientists from the 1927 Solvay Conference."/>
+                {/* <figcaption>Scientists from the 1927 Solvay Conference.</figcaption>
+            </figure> */}
            
         </div>
     )
@@ -22,6 +45,7 @@ import solvay from './images/solvay-conf.jpg';
 
   const useStyles = makeStyles(() => ({
     solvay: {
-        width: "100%"
+        width: 1000,
+        height: 400
     }
   }));
