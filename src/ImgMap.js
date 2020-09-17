f5ca7f7c65fc142e98aaf52bd2540bf64efba31d
John Martin <jrmartin182@gmail.com>
2020-09-17 15:24:56 -0600

    styled figcaption

--- src/ImgMap.js
+++ src/ImgMap.js
@@ -51,7 +51,16 @@ import { getThemeProps } from '@material-ui/styles';
                     className={classes.solvay}
                     useMap="#map-overlay" 
                     alt="A group photo of scientists from the 1927 Solvay Conference."/>
-                <figcaption>Scientists from the 1927 Solvay Conference.</figcaption>
+                <figcaption className={classes.caption}>
+                    Scientists from left to right: 
+                    <br/>
+                        <b>Back row:</b> A. Piccard, E. Henriot, P. Ehrenfest, E. Herzen, Th. de Donder, E. Schrödinger, J. E. Verschaffelt,
+                        W. Pauli, W. Heisenberg, R. H. Fowler, L. Brillouin;
+                    <br/> 
+                        <b>Middle row:</b> P. Debye, M. Knudsen, W.L. Bragg, H. A. Kramers, P. A. M. Dirac, A. H. Compton, L. de Broglie, M. Born, N. Bohr;
+                    <br/> 
+                        <b>Front row:</b> I. Langmuir, M. Planck, M. Curie, H.A . Lorentz, A. Einstein, P. Langevin, Ch.-E. Guye, C. T. R. Wilson, O. W. Richardson
+                </figcaption>
             </figure>
            
         </div>
@@ -63,12 +72,17 @@ import { getThemeProps } from '@material-ui/styles';
         alignSelf: 'center'
       },
     solvay: {
-        width: 1000,
-        height: 400,
+        width: "100%",
+        maxWidth: 1000,
+        maxHeight: 400,
+        height: "100%",
     },
     container: {
         height: '100vh',
         display: 'flex',
         flexDirection: 'column'
+    },
+    caption: {
+        fontSize: 14
     }
   }));
