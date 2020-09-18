d7aadde8d892c0c0073337798bccc58c841776f1
John Martin <jrmartin182@gmail.com>
2020-09-18 17:32:34 -0600

    updated data with profile image web address and coordinates for mapping

--- src/Profile.js
+++ src/Profile.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import temp from './images/temp.jpg';
+import temp from './images/profilePics/temp.jpg';
 import data from './data.json'
+import images from './images/images'
+
+
 
 export default function Profile(props){
-    // const person = props.person
-    const tempPerson = data[0];
-    console.log("temp", tempPerson)
+    const personData = data.find(person => person.name.toLowerCase().includes(props.profile));
+
     const classes = useStyles();
+    const image = images.map(image => <img src={image.src} /> );
+
+
     return (
         <div className={classes.container}>
-            <h2>{tempPerson.name}</h2>
-            {props.profile}
-            {/* Can you dynamically change alt to match real name? */}
-            <img src={temp} className={classes.image} alt="Image of scientist"/>
-    <p>Born: {tempPerson.dob} in {tempPerson.origin}</p>
-            <p>Dicipline: {tempPerson.dicipline}</p>
-            <p>Notable work: {tempPerson.notable}</p>
-            <a href={tempPerson.wiki}>Click here to learn more!</a>
+            <h2>{personData.name}</h2>
+            <img src={temp} className={classes.image} alt={`Scientist: ${personData.name}`}/>
+
+            {image}
+
+    <p>Born: {personData.dob} in {personData.origin}</p>
+            <p>Dicipline: {personData.dicipline}</p>
+            <p>Notable work: {personData.notable}</p>
+            <a href={personData.wiki}>Click here to learn more!</a>
 
         </div>
     )
